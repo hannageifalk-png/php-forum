4051eaaea307f379dc9d921d5c561a57982365f9
Hanna Geifalk <hanna.geifalk@gmail.com>
2026-09-03 10:47:57 +0200

    Remove hardcoded group data

--- html/includes/functions.php
+++ html/includes/functions.php
@@ -1,9 +1,6 @@
 <?php
 function getMyGroups(){
     $groups = array();
-    $groups[] = array("name" => "Group 1", "id" => 1);
-    $groups[] = array("name" => "Group 2", "id" => 2);
-    $groups[] = array("name" => "Group 3", "id" => 3);
     return $groups;
 }
 ?>
